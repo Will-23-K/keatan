fa87cc913c15a7ecd8ca4ea4abad8e9efd20bb5e
Willy <kekaeyingwane@gmail.com>
2025-08-26 10:01:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,10 @@ def recommend():
     rec = recommend_site(job)
     return jsonify(rec)
 
+
+
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8501, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render gives PORT automatically
+    app.run(host="0.0.0.0", port=port, debug=True)
